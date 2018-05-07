d1cfb4275a5af74ece8c79070dc665a032491cad
Naris R <nariscatboy@gmail.com>
2018-05-07 15:30:32 +1000

    implemented gamefeature

---
+++ PoolTable/gamefeature.h
@@ -0,0 +1,14 @@
+#include "poolgame.h"
+
+class GameFeature : public PoolGame {
+public:
+  GameFeature(PoolGame *game) : m_game(game) {}
+  virtual ~GameFeature() { delete m_game; }
+  
+  virtual void draw(QPainter& p) { m_game->draw(p); }
+  virtual void simultateTimeStep(float dtime) { m_game->simulateTimeStep(dtime); }
+  virtual void handleEvent(QEvent e) { m_game->handleEvent(e); }
+  
+private:
+  PoolGame *m_game;
+}

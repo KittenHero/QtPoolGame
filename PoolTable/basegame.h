b8dbfb204c690cb4502256152b66f9515dc53d06
Naris R <nariscatboy@gmail.com>
2018-05-07 15:37:38 +1000

    minor correction

--- PoolTable/basegame.h
+++ PoolTable/basegame.h
@@ -5,10 +5,10 @@ public:
   BaseGame(Table* table, std::vector<Ball*> balls, Ball* cueball) :
   PoolGame(table, balls, cueball) {} 
   ~BaseGame() {}
-  void draw(QPainter*);
-  void simulatedTimeStep(float);
+  void draw(QPainter*) const override;
+  void simulatedTimeStep(float) override;
 
 private:
   void resolveBallCollision(Ball &, Ball &, QVector2D)
   void resolveTableCollision(Ball &, QVector2D)
-}
+};

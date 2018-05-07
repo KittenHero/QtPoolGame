e44cf81224bd820b5fa4bb4cd8003182aab60321
Naris R <nariscatboy@gmail.com>
2018-05-07 15:38:34 +1000

    Update gamefeature.h

--- PoolTable/gamefeature.h
+++ PoolTable/gamefeature.h
@@ -7,7 +7,7 @@ public:
   
   virtual void draw(QPainter& p) { m_game->draw(p); }
   virtual void simultateTimeStep(float dtime) { m_game->simulateTimeStep(dtime); }
-  virtual void handleEvent(QEvent e) { m_game->handleEvent(e); }
+  virtual void handleEvent(QEvent* e) { m_game->handleEvent(e); }
   
 private:
   PoolGame *m_game;

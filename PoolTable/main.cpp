ca2b7915080d0b10c5712efe3b2cf1b4cc13b7f4
Naris <nariscatboy@gmail.com>
2018-05-14 02:44:38 +1000

    search for config in current directory

--- PoolTable/main.cpp
+++ PoolTable/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-	std::unique_ptr<PoolGame> game = Initializer().createPoolgame("../PoolTable/config.json");
+	std::unique_ptr<PoolGame> game = Initializer().createPoolgame("config.json");
 
     //if the returned game is a nullptr the json file couldn't be found or was invalid
 	if (game.get() == nullptr) return a.exec();
